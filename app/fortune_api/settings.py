af6661f0d615e8b58faa1786ad79138841970108
Daniel Rogowski <daniel.rogowski@onet.pl>
2022-06-19 17:44:12 +0200

    🛂 Add cors headers

--- app/fortune_api/settings.py
+++ app/fortune_api/settings.py
@@ -30,18 +30,22 @@ INSTALLED_APPS = [
     "fortune_triggers",
     "fortune_models",
     "fortune_admin",
-    "fortune_prepopulate"
+    "fortune_prepopulate",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'fortune_api.urls'
 
